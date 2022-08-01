c1f6804de3ab512788f6da0787226fb441fc363d
Shiny1814 <45264125+Shiny1814@users.noreply.github.com>
2022-08-01 15:21:58 +0200

    now works for images without raw files

--- selector.py
+++ selector.py
@@ -35,7 +35,7 @@ def copy(imagesToCopy):
         global inputDir
         global outputDir
         copy2(f"{inputDir}{v}", f"{outputDir}{v}")
-        if v != None:
+        if r != None:
             copy2(f"{inputDir}{r}", f"{outputDir}{r}")
     pass
 
